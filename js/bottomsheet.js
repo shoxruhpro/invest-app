7fbc48d47aebe129a160bedc0e888a4986ff8586
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-05-31 14:54:01 +0500

    first commit

---
+++ js/bottomsheet.js
@@ -0,0 +1,85 @@
+/* Created by Tivotal */
+
+let bottomSheet = document.querySelector("#bottom-sheet");
+let overlay = document.querySelector(".overlay");
+let content = document.querySelector(".content");
+let dragIcon = document.querySelector(".drag-icon");
+let defaultHeight = 97;
+
+let isDragging = false,
+  startY,
+  startHeight;
+
+let updateHeight = (height) => {
+  //updating sheet height
+  content.style.height = height ? `${height}vh` : "max-content";
+
+  // if the sheet height is equal to 100 then toggling fullsceen class to bottom sheet
+  bottomSheet.classList.toggle("fullscreen", height === 100);
+};
+
+let showSheet = (height) => {
+  defaultHeight = height;
+  bottomSheet.classList.add("show");
+
+  // updating sheet height with default height 58
+  updateHeight(height);
+
+  document.body.style.overflow = "hidden";
+};
+
+let hideSheet = () => {
+  bottomSheet.classList.remove("show");
+  document.body.style.overflow = "auto";
+  document
+    .querySelectorAll("#bottom-sheet > .content > .body > section")
+    .forEach((elem) => {
+      elem.style.display = "none";
+    });
+};
+
+let dragStart = (e) => {
+  isDragging = true;
+  bottomSheet.classList.add("dragging");
+  //recording intitial y position and sheet height
+  startY = e.pageY || e.touches?.[0].pageY;
+  startHeight = parseInt(content.style.height);
+};
+
+let dragging = (e) => {
+  //return if isDragging is false
+  if (!isDragging) return;
+
+  //calculating new height of sheet by using starty and start height
+  //calling updateHeight function with new height as argument
+
+  let delta = startY - (e.pageY || e.touches?.[0].pageY);
+  let newHeight = startHeight + (delta / window.innerHeight) * 100;
+
+  updateHeight(newHeight);
+};
+
+let dragStop = () => {
+  isDragging = false;
+  bottomSheet.classList.remove("dragging");
+
+  //setting sheet height based on the sheet current height or position
+  let sheetHeight = parseInt(content.style.height);
+
+  sheetHeight < ~~(defaultHeight / 2)
+    ? hideSheet()
+    : sheetHeight > defaultHeight
+    ? updateHeight(100)
+    : updateHeight(defaultHeight);
+  //if height is greater than 75 making sheet full screen else making it to 58vh
+};
+
+dragIcon.addEventListener("mousedown", dragStart);
+dragIcon.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+dragIcon.addEventListener("touchstart", dragStart);
+dragIcon.addEventListener("touchmove", dragging);
+document.addEventListener("touchend", dragStop);
+
+overlay.addEventListener("click", hideSheet);
---
+++ js/main.js
@@ -0,0 +1,57 @@
+const cardInBottomSheet = document.querySelector("#bottom-sheet .card");
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("main.container").style.display = "block";
+  document.querySelector("nav").style.display = "flex";
+  document.querySelector(".loading").style.display = "none";
+});
+
+document.querySelector("#faq-btn").addEventListener("click", () => {
+  document.querySelector("#faq-content").style.display = "block";
+  cardInBottomSheet.style.display = "none";
+  showSheet(80);
+});
+
+document.querySelector("#invest-btn")?.addEventListener("click", () => {
+  document.querySelector("#invest-content").style.display = "block";
+  cardInBottomSheet.style.display = "block";
+  showSheet(80);
+});
+
+document.querySelector("#withdraw-btn").addEventListener("click", () => {
+  document.querySelector("#withdraw-content").style.display = "block";
+  cardInBottomSheet.style.display = "block";
+  showSheet(97);
+});
+
+document.querySelector("#withdraw-nav-btn").addEventListener("click", () => {
+  document.querySelector("#withdraw-content").style.display = "block";
+  cardInBottomSheet.style.display = "block";
+  showSheet(97);
+});
+
+document.querySelector("#topup-btn").addEventListener("click", () => {
+  document.querySelector("#topup-content").style.display = "block";
+  cardInBottomSheet.style.display = "block";
+  showSheet(75);
+});
+
+document.querySelector("#topup-nav-btn").addEventListener("click", () => {
+  document.querySelector("#topup-content").style.display = "block";
+  cardInBottomSheet.style.display = "block";
+  showSheet(75);
+});
+
+/**
+ * @param {Element} elem
+ */
+const handleTabChange = (elem, order) => {
+  document.querySelectorAll("#tabbar > div").forEach((elem) => {
+    elem.classList.remove("active-tab");
+  });
+  elem.classList.add("active-tab");
+  document.querySelectorAll("#tab-contents > div").forEach((elem) => {
+    elem.style.display = "none";
+  });
+  document.querySelector("#tab-content-" + order).style.display = "block";
+};
